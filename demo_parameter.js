4bdc270ed9ffe2036cabafc330e7bd4abf011030
Silas <sisilambros@gmail.com>
2018-07-13 15:41:58 -0700

    change

---
+++ demo_parameter.js
@@ -0,0 +1,13 @@
+let my_parameter = process.argv[2];
+if (my_parameter> 5)
+console.log ('The value is greater than 5');
+// Print the my_parameter variable
+if(my_parameter < 5){
+    console.log()
+
+if(my_parameter<5)
+console.log('The value is less than 5')
+//Print the my_paramter variable
+if(my_parameter>5)
+console.log()
+}
